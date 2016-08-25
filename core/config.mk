9bbc2fa6730ebf30b0be4ff90d7af87a3fc277d4
Joe Maples <joe@frap129.org>
2016-08-25 14:20:52 +0000

    Benzo to Maple
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Conflicts:
    	core/product_config.mk
    	envsetup.sh
    
    Signed-off-by: Joe Maples <joe@frap129.org>

--- core/config.mk
+++ core/config.mk
@@ -882,7 +882,7 @@ endif
 ifneq ($(CUSTOM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/benzo/sepolicy/sepolicy.mk)
+$(eval include vendor/maple/sepolicy/sepolicy.mk)
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
